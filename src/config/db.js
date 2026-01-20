7729e23f92828a967f3eef54de2d8be644297251
Alberto <betim009@gmail.com>
2026-01-20 20:36:54 -0300

    update

--- src/config/db.js
+++ src/config/db.js
@@ -1,9 +1,11 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+dotenv.config();
 
 async function connectDB() {
   const uri = process.env.MONGODB_URI;
   if (!uri) {
-    throw new Error('MONGODB_URI is not defined');
+    throw new Error("MONGODB_URI is not defined");
   }
 
   await mongoose.connect(uri);
